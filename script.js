866d00428705d87f290709001f296fc7c2ce1d0d
LEXONOS <louismcrenault@gmail.com>
2026-08-14 16:54:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -149,7 +149,7 @@
 
   /* ---- Apparition au scroll ---- */
   var targets = document.querySelectorAll(
-    '.shead, .stat, .cpanel, .compare__arrow, .place, .lifband__card, .mcard, .realstrip, .step, .seg, .plan, .buynote, .plans__note, .law__card, .qa, .cta'
+    '.shead, .stat, .cpanel, .compare__arrow, .place, .lifband__card, .mcard, .realstrip, .step, .seg, .plan, .buynote, .plans__note, .lterms, .law__card, .qa, .cta'
   );
   if (reduced || !('IntersectionObserver' in window)) {
     targets.forEach(function (el) { el.classList.add('in'); });
